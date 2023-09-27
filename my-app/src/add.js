0d9b8e1c79d1fab76ef6b1d55761a4b85cf9a142
Joseph <jgrados@ggc.edu>
2023-09-27 14:55:41 -0500

    New testing code and json files

--- my-app/src/add.js
+++ my-app/src/add.js
@@ -1,4 +1,21 @@
 function add(a, b){
     return a + b;
 }
-module.exports = add;
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+module.exports = {
+    add,
+    subtract,
+    multiply,
+    divide
+}
