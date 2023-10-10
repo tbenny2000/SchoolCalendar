cf2810350d979503ae27619427c6b6bbda70bfa2
rmelendez195 <114716780+rmelendez195@users.noreply.github.com>
2023-10-09 20:42:38 -0400

    Firebase Sign in
    Updated Login.js to use our Firebase API file to sign in user. Returns succesful sign in, if credentials are right and error message if wrong.

--- bear-track/src/HomePage.js
+++ bear-track/src/HomePage.js
@@ -1,56 +1,57 @@
-class HomePage{
-    constructor(){
-        this.currentTime = null;
-        this.selectedDate = null;
-        this.calendar = null;
-
-        this.render();
+class HomePage {
+    constructor() {
+      this.currentTime = null;
+      this.selectedDate = null;
+      this.calendar = null;
+  
+      this.render();
     }
-
-    render(){
-        const context = document.getElementById('content');
-        //Creating a button to confirm the time of day
-        const timeButton = document.createElement('button');
-        timeButton.addEventListener('click', () =>  this.confirmTimeOfDay());
-        context.appendChild(timeButton);
-
-        this.UpdateTimeButtonText(timeButton);
-
-        const newCalendarButton = document.createElement('button');
-        newCalendarButton.textContent = 'New Calendar';
-        newCalendarButton.addEventListener('click', () => this.createNewCalendar());
-        content.appendChild(newCalendarButton);
+  
+    render() {
+      const content = document.getElementById('content'); // Fix variable name 'content'
+  
+      // Creating a button to confirm the time of day
+      const timeButton = document.createElement('button');
+      timeButton.addEventListener('click', () => this.confirmTimeOfDay());
+      content.appendChild(timeButton); // Fix variable name 'content'
+  
+      this.updateTimeButtonText(timeButton); // Fix function name 'updateTimeButtonText'
+  
+      const newCalendarButton = document.createElement('button');
+      newCalendarButton.textContent = 'New Calendar';
+      newCalendarButton.addEventListener('click', () => this.createNewCalendar());
+      content.appendChild(newCalendarButton); // Fix variable name 'content'
     }
-
-    UpdateTimeButtonText(button){
-        if(this.currentTime){
-            button.textContent = '${this.currentTime}';
-        }else{
-            button.textContent = 'Select Time of Day: ';
-        }
+  
+    updateTimeButtonText(button) {
+      if (this.currentTime) {
+        button.textContent = `${this.currentTime}`;
+      } else {
+        button.textContent = 'Select Time of Day: ';
+      }
     }
-
-    confirmTimeOfDay(){
-        //Setting time vars for the function
-        const now = new Date();
-        const hours = now.getHours();
-        const minutes = now.getMinutes();
-
-        //Formatting the time as HH:MM
-        this.currentTime = `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
-
-        //Update the time button text message
-        const timeButton = document.querySelector('button');
-        this.UpdateTimeButtonText(timeButton);
-
-
-        console.log('Time of day confirmed: ', this.currentTime);
-
+  
+    confirmTimeOfDay() {
+      // Setting time vars for the function
+      const now = new Date();
+      const hours = now.getHours();
+      const minutes = now.getMinutes();
+  
+      // Formatting the time as HH:MM
+      this.currentTime = `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
+  
+      // Update the time button text message
+      const timeButton = document.querySelector('button');
+      this.updateTimeButtonText(timeButton); // Fix function name 'updateTimeButtonText'
+  
+      console.log('Time of day confirmed: ', this.currentTime);
     }
-
-    createNewCalendar(){
-        //Code to insert calendar from html file
-        console.log('Creating a new calendar from user input');
+  
+    createNewCalendar() {
+      // Code to insert calendar from html file
+      console.log('Creating a new calendar from user input');
     }
-}
-const homepage = new HomePage();
+  }
+  
+  const homepage = new HomePage();
+  
--- bear-track/src/components/Login.js
+++ bear-track/src/components/Login.js
@@ -1,52 +1,57 @@
 import React, { useState } from 'react';
 import './Login.css'; // Import the CSS file for styling
-import firebase from '../firebase.js'
+import firebase from '../firebase'; // Import your firebase.js file
 
-// To allow a user to Log into an existing account.
-// It does not sign you in if you do not have an account
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
 
-  const Login = () => {
-    
-      const [email, setEmail] = useState('');
-      const [password, setPassword] = useState('');
-    
-      const handleSignIn = async () => {
-        firebase.auth().signInWithEmailAndPassword(email, password)
-            .then((userCredential) => {
-                // Signed in
-                var user = userCredential.user;
-            
-                // ...
-            })
-            .catch((error) => {
-                var errorCode = error.code;
-                var errorMessage = error.message;
-            });
-      };
-      
-    return (
-      <div className="login-container">
-        <div className="login-left">
+  const handleSignIn = async () => {
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+      setSuccessMessage('Sign-in successful!'); // Set a success message
+    } catch (error) {
+      setError(error.message);
+    }
+  };
 
-         {/* Insert your image here */}
+  return (
+    <div className="login-container">
+      <div className="login-left">
+        {/* Insert your image here */}
         <img src="/BearLogo.png" alt="Your Image" />
-
-          <h2>Login</h2>
-          <form>
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-
-            <button onClick={handleSignIn}>Login</button>
-          </form>
-        </div>
-        <div className="login-right">
-          {/* Background image will be displayed here */}
-        </div>
+        <h2>Login</h2>
+        <form>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          {error && <p className="error-message">{error}</p>}
+          {successMessage && <p className="success-message">{successMessage}</p>}
+          <button type="button" onClick={handleSignIn}>Login</button>
+        </form>
       </div>
-    );
-}
+      <div className="login-right">
+        {/* Background image will be displayed here */}
+      </div>
+    </div>
+  );
+};
 
 export default Login;
