4e2235d654b271d9511ec07c9b6c336c865f3f44
Muketoi Celestino <muketoicelestino2019@gmail.com>
2023-10-13 01:40:08 -0400

    Modified files to match original, and kept firebase config for firestore.

--- bear-track/src/index.js
+++ bear-track/src/index.js
@@ -2,19 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
 import reportWebVitals from './reportWebVitals';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-
-const router = createBrowserRouter([
-  { path: '/', element: <App />},
-  
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <App />
   </React.StrictMode>
 );
 
