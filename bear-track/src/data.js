f6aa27317f7f496d5b8407810200196ac4bc666a
Ben Tacota <tbenny2000@yahoo.com>
2023-10-15 03:06:40 -0400

    created the data as a json file

---
+++ bear-track/src/data.js
@@ -0,0 +1,356 @@
+const data = {
+    title: "ScheduleSynScholars",
+    description: "calendar app for GGC students",
+    users: [
+    {
+        "id":"900264732",
+        "firstName": "Allice",
+        "lastName": "Jonson",
+        "email": "ajohnson@ggc.edu",
+        "passowrd": "VEkvMu3k"
+    },
+    {
+        "id":"900325651",
+        "firstName": "Benjamin",
+        "lastName": "Parker",
+        "email": "bparker@ggc.edu",
+        "passowrd": "v6TsxTGR"
+    },
+    {
+        "id":"900792188",
+        "firstName": "Charlotte",
+        "lastName": "Adams",
+        "email": "cadams@ggc.edu",
+        "passowrd": "wu6AYLKD"
+    },
+    {
+        "id":"900151866",
+        "firstName": "Daniel",
+        "lastName": "Mitchell",
+        "email": "dmitchell@ggc.edu",
+        "passowrd": "vZYs9vAY"
+    },
+    {
+        "id":"900199224",
+        "firstName": "Emily",
+        "lastName": "Wilson",
+        "email": "ewilson@ggc.edu",
+        "passowrd": "5yVMEZaD"
+    },
+    {
+        "id":"900930880",
+        "firstName": "Frank",
+        "lastName": "Anderson",
+        "email": "fanderson@ggc.edu",
+        "passowrd": "ZBzRpfBx"
+    },
+    {
+        "id":"900713844",
+        "firstName": "Grace",
+        "lastName": "Turner",
+        "email": "gturner@ggc.edu",
+        "passowrd": "NxLGSk6V"
+    },
+    {
+        "id":"900020784",
+        "firstName": "Henry",
+        "lastName": "Roberts",
+        "email": "hroberts@ggc.edu",
+        "passowrd": "55gxq4ve"
+    },
+    {
+        "id":"900228778",
+        "firstName": "Isabella",
+        "lastName": "Hayes",
+        "email": "ihayes@ggc.edu",
+        "passowrd": "9HvLtgva"
+    },
+    {
+        "id":"900189102",
+        "firstName": "Jack",
+        "lastName": "Davis",
+        "email": "jdavis@ggc.edu",
+        "passowrd": "avXdcDhC"
+    },
+    {
+        "id":"900208448",
+        "firstName": "Katherine",
+        "lastName": "Lewis",
+        "email": "klewis@ggc.edu",
+        "passowrd": "HeuApdfY"
+    },
+    {
+        "id":"900348378",
+        "firstName": "Liam",
+        "lastName": "Thompson",
+        "email": "lthompson@ggc.edu",
+        "passowrd": "AA5Ag5N6"
+    },
+    {
+        "id":"900833552",
+        "firstName": "Mia",
+        "lastName": "Murphy",
+        "email": "mmurphy@ggc.edu",
+        "passowrd": "5xaqap9Z"
+    },
+    {
+        "id":"900628973",
+        "firstName": "Noah",
+        "lastName": "Foster",
+        "email": "nfoster@ggc.edu",
+        "passowrd": "NBHC7g7v"
+    },
+    {
+        "id":"900294544",
+        "firstName": "Olivia",
+        "lastName": "Brooks",
+        "email": "obrooks@ggc.edu",
+        "passowrd": "a5wu3tFK"
+    },
+    {
+        "id":"900558448",
+        "firstName": "Patrick",
+        "lastName": "Ward",
+        "email": "pward@ggc.edu",
+        "passowrd": "xe8WzKcz"
+    },
+    {
+        "id":"900226141",
+        "firstName": "Quinn",
+        "lastName": "Harrison",
+        "email": "qharrison@ggc.edu",
+        "passowrd": "W5TPLGXu"
+    },
+    {
+        "id":"900852531",
+        "firstName": "Rachel",
+        "lastName": "Bell",
+        "email": "rbell@ggc.edu",
+        "passowrd": "re8qgfAu"
+    },
+    {
+        "id":"900064623",
+        "firstName": "Samuel",
+        "lastName": "Gray",
+        "email": "sgray@ggc.edu",
+        "passowrd": "rn9WLzFN"
+    },
+    {
+        "id":"900972674",
+        "firstName": "Taylor",
+        "lastName": "Jenkins",
+        "email": "tjenkins@ggc.edu",
+        "passowrd": "ptaWpZzd"
+    },
+    {
+        "id":"900694252",
+        "firstName": "Victoria",
+        "lastName": "Carter",
+        "email": "vcarter@ggc.edu",
+        "passowrd": "c6PFmxnj"
+    },
+    {
+        "id":"900320195",
+        "firstName": "William",
+        "lastName": "Smith",
+        "email": "wsmith@ggc.edu",
+        "passowrd": "Sra3vbjw"
+    },
+    {
+        "id":"900614415",
+        "firstName": "Zoey",
+        "lastName": "Foster",
+        "email": "zfoster@ggc.edu",
+        "passowrd": "bVDddG6H"
+    },
+    {
+        "id":"900264975",
+        "firstName": "Alexander",
+        "lastName": "White",
+        "email": "awhite@ggc.edu",
+        "passowrd": "rU8Pd2qE"
+    },
+    {
+        "id":"900494923",
+        "firstName": "Brooke",
+        "lastName": "Martin",
+        "email": "bmartin@ggc.edu",
+        "passowrd": "M3CwREU7"
+    },
+    {
+        "id":"900915185",
+        "firstName": "Christopher",
+        "lastName": "Walker",
+        "email": "cwalker@ggc.edu",
+        "passowrd": "ambDEsXF"
+    },
+    {
+        "id":"900039985",
+        "firstName": "Daisy",
+        "lastName": "King",
+        "email": "dking@ggc.edu",
+        "passowrd": "jKWUSFnG"
+    },
+    {
+        "id":"900581347",
+        "firstName": "Ethan",
+        "lastName": "Turner",
+        "email": "eturner@ggc.edu",
+        "passowrd": "VPhTRbzG"
+    },
+    {
+        "id":"900932111",
+        "firstName": "Fiona",
+        "lastName": "Green",
+        "email": "fgreen@ggc.edu",
+        "passowrd": "9CUK6RZL"
+    },
+    {
+        "id":"900255276",
+        "firstName": "George",
+        "lastName": "Mitchell",
+        "email": "gmitchell@ggc.edu",
+        "passowrd": "afptvJXp"
+    },
+    {
+        "id":"900314499",
+        "firstName": "Hannah",
+        "lastName": "Baker",
+        "email": "hbaker@ggc.edu",
+        "passowrd": "6D7c9Crn"
+    },
+    {
+        "id":"900504781",
+        "firstName": "Isaac",
+        "lastName": "Reed",
+        "email": "ireed@ggc.edu",
+        "passowrd": "EYBAGzyz"
+    },
+    {
+        "id":"900607829",
+        "firstName": "Jasmine",
+        "lastName": "Hall",
+        "email": "jhall@ggc.edu",
+        "passowrd": "sPHZkKw8"
+    },
+    {
+        "id":"900573399",
+        "firstName": "Kevin",
+        "lastName": "Turner",
+        "email": "kturner@ggc.edu",
+        "passowrd": "hUBABTP8"
+    },
+    {
+        "id":"900388185",
+        "firstName": "Lily",
+        "lastName": "Adams",
+        "email": "ladams@ggc.edu",
+        "passowrd": "rWpXXt8p"
+    },
+    {
+        "id":"900254651",
+        "firstName": "Michael",
+        "lastName": "Brown",
+        "email": "mbrown@ggc.edu",
+        "passowrd": "arm96THW"
+    },
+    {
+        "id":"900254652",
+        "firstName": "Natalie",
+        "lastName": "Parker",
+        "email": "nparker@ggc.edu",
+        "passowrd": "UBtmKGxY"
+    },
+    {
+        "id":"900441075",
+        "firstName": "Owen",
+        "lastName": "Bennett",
+        "email": "obennett@ggc.edu",
+        "passowrd": "6wUnxwmz"
+    },
+    {
+        "id":"900203452",
+        "firstName": "Penelope",
+        "lastName": "Clark",
+        "email": "pclark@ggc.edu",
+        "passowrd": "mmLNjAKK"
+    },
+    {
+        "id":"900086033",
+        "firstName": "Quentin",
+        "lastName": "Hayes",
+        "email": "qhayes@ggc.edu",
+        "passowrd": "94EECnDE"
+    },
+    {
+        "id":"900839849",
+        "firstName": "Ruby",
+        "lastName": "Wilson",
+        "email": "rwilson@ggc.edu",
+        "passowrd": "QphR7Mr6"
+    },
+    {
+        "id":"900753393",
+        "firstName": "Sebastian",
+        "lastName": "Hill",
+        "email": "shill@ggc.edu",
+        "passowrd": "CeCBZDNj"
+    },
+    {
+        "id":"900073373",
+        "firstName": "Taylor",
+        "lastName": "Evans",
+        "email": "tevans@ggc.edu",
+        "passowrd": "Hgk77hvx"
+    },
+    {
+        "id":"900036159",
+        "firstName": "Ulysses",
+        "lastName": "Roberts",
+        "email": "uroberts@ggc.edu",
+        "passowrd": "hK9rssWL"
+    },
+    {
+        "id":"900473416",
+        "firstName": "Violet",
+        "lastName": "Campbell",
+        "email": "vcampbell@ggc.edu",
+        "passowrd": "gDKKJ8KR"
+    },
+    {
+        "id":"900574033",
+        "firstName": "Wlater",
+        "lastName": "Morris",
+        "email": "wmorris@ggc.edu",
+        "passowrd": "qX5Mbb2w"
+    },
+    {
+        "id":"900610443",
+        "firstName": "Xander",
+        "lastName": "Reed",
+        "email": "xreed@ggc.edu",
+        "passowrd": "PD9JuvVN"
+    },
+    {
+        "id":"900291788",
+        "firstName": "Yasmine",
+        "lastName": "Martinez",
+        "email": "ymartinez@ggc.edu",
+        "passowrd": "xUaDEVaF"
+    },
+    {
+        "id":"900029169",
+        "firstName": "Zachary",
+        "lastName": "Johnson",
+        "email": "zjohnson@ggc.edu",
+        "passowrd": "xXvseZNx"
+    },
+    {
+        "id":"900554380",
+        "firstName": "Zoe",
+        "lastName": "Anderson",
+        "email": "zanderson@ggc.edu",
+        "passowrd": "cxfSdSNV"
+    }
+],
+};
