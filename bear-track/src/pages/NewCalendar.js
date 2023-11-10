982d2d57450315cf54c257c5b24b205e6647ee66
Joseph <jgrados@ggc.edu>
2023-11-09 22:58:28 -0500

    Adding people feature functionality completed

--- bear-track/src/pages/NewCalendar.js
+++ bear-track/src/pages/NewCalendar.js
@@ -13,9 +13,15 @@ const NewCalendar = () =>{
   const [userID, setUserID] = useState('');
   const [email, setEmailAddress] = useState('');
   const [image, setImage] = useState("");
+  const [calendarName, setCalendarName] = useState('');
+
+  const [amountOfEnteredUsers, setAmountOfEnteredUsers] = useState(new Set());
+  const [limitMessage, setLimitMessage] = useState(false);
 
   const [usernameToCheck, setUsernameToCheck] = useState('');
+  const [emailToCheck, setEmailToCheck] = useState('');
 
+  const [addMessage, setAddMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [isShaking, setIsShaking] = useState(false);
 
@@ -28,6 +34,7 @@ const NewCalendar = () =>{
       }
     });
 
+    
     return () => unregisterAuthObserver();
   }, []);
 
@@ -42,7 +49,7 @@ const NewCalendar = () =>{
           console.log('Printing from userDoc: ', userDoc.data);
           const uName = userDoc.data().userName;
           const userID = userDoc.data().userID;
-          const eAddress = userDoc.data().email;
+          const eAddress = userDoc.data().emailAddress;
           const img = userDoc.data().imageURL;
 
           setUserName(uName);
@@ -74,29 +81,6 @@ const NewCalendar = () =>{
 
     // setUsernameToCheck(value);
 
-    // // Query the Firestore collection for a document with the specified username
-    // firestore.collection('users')
-    //   .where('emailAddress', '==', value)
-    //   .get()
-    //   .then((querySnapshot) => {
-    //     if (querySnapshot.size > 0) {
-    //       // A document with the specified username exists
-        
-    //       console.log("User document exists for username:", usernameToCheck);
-    //       // You can access the document using querySnapshot.docs[0]
-    //       const userDocument = querySnapshot.docs[0].data();
-    //     } else {
-    //       // No document with the specified username exists
-    //       console.log(usernameToCheck);
-    //       console.log("User document does not exist for username:", usernameToCheck);
-        
-    //     }
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error checking for user document:", error);
-        
-    //   });
-
     //To reset error message and animation
     setErrorMessage('');
     setIsShaking(false);
@@ -106,7 +90,39 @@ const NewCalendar = () =>{
       const value = e.target.value;
       setInputValue(value);
       setUsernameToCheck(e.target.value);
+      setEmailToCheck(e.target.value);
 
+
+      //Checking that user doesn't enter their own info when creating calendar
+      //But if they do then display an error message
+      if(value === userName || value === email){
+        console.log("User email: ", email);
+        setErrorMessage("You can't enter your own information.");
+        setIsShaking(true);
+        return;
+      }
+
+      //Checking if the user info has already been submitted
+      if(amountOfEnteredUsers.has(value)){
+        console.log("Amount of entered users: ", amountOfEnteredUsers);
+        setErrorMessage("You already entered this user info. Try enter someone else.");
+        setIsShaking(true);
+        setLimitMessage(true);
+
+        setTimeout(() => {
+          setLimitMessage(false);
+        }, 5000);
+        return;
+      }
+
+      //This is to check if an when the user has exceed their limit in adding people
+      //It's because like array/arraylist it starts in 0 and counting so 4 but it's 5 numberically
+      if(amountOfEnteredUsers.size > 4){
+        setErrorMessage("You have reached your limit in adding people please create calendar.");
+        setIsShaking(true);
+        setLimitMessage(true);
+        return;
+      }
       
     // Query the Firestore collection for a document with the specified username
     firestore.collection('users')
@@ -116,16 +132,45 @@ const NewCalendar = () =>{
         if (querySnapshot.size > 0) {
           // A document with the specified username exists
         
-          console.log("User document exists for username:", usernameToCheck);
+          console.log("User document exists for username", value);
           // You can access the document using querySnapshot.docs[0]
           const userDocument = querySnapshot.docs[0].data();
+          //Add users to the amountOfEnteredUsers 
+          setAmountOfEnteredUsers((prevSet) => new Set(prevSet).add(value));
+          setAddMessage('Person Added!');
+          setInputValue('');
+
+          setTimeout(()=>{
+            setAddMessage('');
+          }, 5000);
         } else {
-          // No document with the specified username exists
-          console.log(usernameToCheck);
-          console.log("User document does not exist for username:", usernameToCheck);
-          setErrorMessage('Please enter an actual email or username!')
-        setIsShaking(true);
+          // No document with the specified username, now checks if email exists
+          firestore.collection('users')
+          .where('emailAddress', '==', value)
+          .get()
+          .then((emailQuerySnapshot) => {
+            if (emailQuerySnapshot.size > 0){
+              console.log("User document does not exist for email:", value);
+              const emailUserDocument = emailQuerySnapshot.docs[0].data();
+              //Add the user to the amountOfEnteredUsers
+              setAmountOfEnteredUsers((prevSet) => new Set(prevSet).add(value));
+              setAddMessage('Person Added!');
+              setInputValue('');
+
+              setTimeout(()=>{
+                setAddMessage('');
+              }, 5000);
+            } else{
+              //If there no document with store username or email
+              console.log("User document does not exist for username or email", value);
+              setErrorMessage('Please enter an actual email or username!')
+              setIsShaking(true);
         
+            }
+          })
+          .catch((error) =>{
+            console.error("Error checking for email: ", error);
+          });
         }
       })
       .catch((error) => {
@@ -238,7 +283,8 @@ const NewCalendar = () =>{
           className = {isShaking ? 'shake' : ''}
           ></input>
           {errorMessage && <div className = "error-message">{errorMessage}</div>}
-
+          {limitMessage && <div className = "limit-message">{limitMessage}</div>}
+          {addMessage && <div className = "add-message">{addMessage}</div>}
         </div>
         </div>
         <button className = "create-btn" onClick={handleCreate}>
@@ -252,5 +298,4 @@ const NewCalendar = () =>{
   );
 }
 
-//export default ViewCalendar;
 export default NewCalendar;
